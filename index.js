5a85dbcaeb2616f3ea76700d54827d1e11c7ed82
yuukinht <50871323+yuukinht@users.noreply.github.com>
2020-01-21 16:59:43 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+//var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(3000);
+var io = require('socket.io')(server);
+
+io.on('connection',
+	function (socket) {
+		socket.on("send",function(data){
+			console.log(data);
+			socket.broadcast.emit("receive",data);
+		});
+	}
+);
